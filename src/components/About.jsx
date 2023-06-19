9d7f8be33c9158a6b2475b6f4be2de60f3dc3ff6
YogeshMun0304 <techyogimun@gmail.com>
2023-06-19 15:43:10 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const About = () => {
+  return (
+    <div name="about" className="w-full h-screen  text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+        </div>
+        <p className="text-xl mt-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ipsa eaque ut nostrum nam temporibus tempora assumenda inventore itaque impedit culpa voluptas sint ea reprehenderit excepturi ipsam a quaerat modi nisi quidem voluptatibus explicabo, delectus possimus! Quaerat asperiores fugit reiciendis?
+
+        </p>
+        <br />
+        <p className="text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quas, repudiandae omnis architecto dolore iste repellendus cumque libero ad nam fugiat at cum amet dicta ipsam accusantium, vel voluptas in rerum adipisci. Excepturi dolorum non delectus exercitationem eius pariatur explicabo inventore velit, fuga itaque nihil sapiente at commodi iure voluptatibus.
+
+        </p>
+
+      </div>
+    </div>
+  );
+};
+
+export default About;
